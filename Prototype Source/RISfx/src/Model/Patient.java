3d0c5f9459a0dc8b3d91f7b4a3470c2cd715bc3d
AlecCromer <33764772+AlecCromer@users.noreply.github.com>
2019-03-05 17:00:44 -0500

    Add files via upload

---
+++ Prototype Source/RISfx/src/Model/Patient.java
@@ -0,0 +1,73 @@
+package Model;
+
+public class Patient {
+
+    String  firstname, lastname, dob, sex, pnumber, email;
+    int patientID;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getPnumber() {
+        return pnumber;
+    }
+
+    public void setPnumber(String pnumber) {
+        this.pnumber = pnumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(int patientID) {
+        this.patientID = patientID;
+    }
+
+    public Patient(int PatientID, String firstname, String lastname, String DoB, String Sex, String pnumber, String email){
+        this.patientID = PatientID;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.dob = DoB;
+        this.sex = Sex;
+        this.pnumber = pnumber;
+        this.email = email;
+    }
+}
