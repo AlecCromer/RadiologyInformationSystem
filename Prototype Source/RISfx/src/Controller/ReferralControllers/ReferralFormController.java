f1a6093d9d507ad29d0608e3139e27b0d1595447
kdmitc9478 <42586024+kdmitc9478@users.noreply.github.com>
2019-03-18 09:15:53 -0400

    Referral Form Started

--- Prototype Source/RISfx/src/Controller/ReferralControllers/ReferralFormController.java
+++ Prototype Source/RISfx/src/Controller/ReferralControllers/ReferralFormController.java
@@ -1,13 +1,59 @@
 package Controller.ReferralControllers;
 
 import Controller.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class ReferralFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class ReferralFormController implements Initializable {
+
+      ////////////////////////
+     //Variable Declaration//
+    ////////////////////////
+    @FXML TextField patentFirstName,    patientLastName,        patientDoB,             patientStreet,
+                    patientCity,        patientState,           patientZip,             patientPhone,
+                    patientEmail,       patientInsuranceNumber, patientPolicyNumber,    patientVitals;
+
+    @FXML ComboBox<String>  procedureBox;
+    @FXML TextArea          referralSummary;
+
+      ////////////////
+     //Initializers//
+    ////////////////
     public static void setView() throws Exception{
         Main.setPopupWindow("ReferralViews/ReferralForm.fxml");
         Main.popup.setResizable(false);
         Main.popup.setHeight(550);
         Main.popup.setWidth(600);
     }
+
+    public void initialize(URL url, ResourceBundle arg1) {
+
+    }
+
+    private void updateTable() {
+
+    }
+
+
+      ///////////////////
+     //List Generators//
+    ///////////////////
+
+
+      //////////////////
+     //Button Methods//
+    //////////////////
+
+
+      ///////////////////
+     //Form Validation//
+    ///////////////////
+
+
 }
