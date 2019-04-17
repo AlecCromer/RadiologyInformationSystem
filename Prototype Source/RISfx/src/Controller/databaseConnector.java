59d92377b1991ed1aa1be4d24b493c87aa03c21b
Alexander Cromer <33764772+AlecCromer@users.noreply.github.com>
2019-04-16 23:13:32 -0400

    Update databaseConnector.java

--- Prototype Source/RISfx/src/Controller/databaseConnector.java
+++ Prototype Source/RISfx/src/Controller/databaseConnector.java
@@ -10,11 +10,19 @@ public class databaseConnector {
     private static final String PASSWORD = "";
     private static final String CONN_STRING = "jdbc:mysql://localhost:3306/RIS";
 
-    // connection  method that connects us to the MySQL database
+
+    /**
+     * connection  method that connects us to the MySQL database
+     * @return Connection returns the Driver Manager Connection
+     */
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
     }
 
+    /**
+     * initializes the connection to the database
+     * @return Connection This returns sum of numA and numB.
+     */
     public static Connection getStartConnection() throws SQLException {
         System.out.println("Connecting to Database...");
         Connection conn = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
@@ -22,6 +30,11 @@ public class databaseConnector {
         return conn;
     }
 
+    /**
+     * This method is used to add two integers. This is
+     * @param ex Takes the SQL exception
+     * @return void Prints out the errors returned by the SQL
+     */
     public static void displayException(SQLException ex) {
         System.err.println("Error Message: " + ex.getMessage());
         System.err.println("Error Code: " + ex.getErrorCode());
